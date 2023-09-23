cc4c9ee2c7492668c90b156730ac88fd6118daf9
DenShtephen <135002535+DenShtephen@users.noreply.github.com>
2023-09-23 16:02:57 +0300

    Update 4.ts

--- src/4.ts
+++ src/4.ts
@@ -1,11 +1,66 @@
+class Key {
+  private signature: number;
+
+  constructor() {
+    this.signature = Math.random();
+  }
+
+  getSignature(): number {
+    return this.signature;
+  }
+}
+
+class Person {
+  private key: Key;
+
+  constructor(key: Key) {
+    this.key = key;
+  }
+
+  getKey(): Key {
+    return this.key;
+  }
+}
+
+abstract class House {
+  door: boolean = false;
+  key: Key;
+  tenants: Person[] = [];
+
+  constructor(key: Key) {
+    this.key = key;
+  }
+
+  abstract OpenDoor(key: Key): void;
+
+  comeIn(tenants: Person): void {
+    if (this.door) {
+      this.tenants.push(person);
+      console.log(`${person.getKey().getSignature()} Hello our new neighbor`);
+    } else {
+      console.log("Door is LOCKED");
+    }
+  }
+}
+
+class MyHouse extends House {
+  OpenDoor(key: Key): void {
+    if (key.getSignature() === this.key.getSignature()) {
+      this.door = true;
+      console.log("You unlocked the door");
+    } else {
+      console.log("Maybe you have invalid key?");
+    }
+  }
+}
+
 const key = new Key();
 
 const house = new MyHouse(key);
 const person = new Person(key);
 
-house.openDoor(person.getKey());
+house.OpenDoor(person.getKey());
 
 house.comeIn(person);
-
 
 export {};
